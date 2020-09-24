d35041a00e2e0d9743b110e7b51375281e93c501
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-09-24 00:54:24 -0600

    Creacion de la clase conexion a Sql Server.

---
+++ scc/Pacientes/Paciente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scc.Pacientes
+{
+    class Paciente
+    {
+    }
+}
