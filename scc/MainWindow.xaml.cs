ae1593e54ac3959771471676cbe83430b0ee2f9c
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-09-27 01:40:38 -0600

    Funcionalidad de iniciar session.

--- scc/MainWindow.xaml.cs
+++ scc/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using scc.Usuarios;
 
 namespace scc
 {
@@ -32,9 +35,87 @@ namespace scc
 
         private void btnInisiarSesion_Click(object sender, RoutedEventArgs e)
         {
-            // Abrir el formulario del menu
-            Menu abrirMenu = new Menu();
-            abrirMenu.Show();
+            // Crear la conexion
+            Conexion conexion = new Conexion(@"(local)\sqlexpress", "scc");
+            // Llamar la clase
+            Usuarios.Usuario usuario = new Usuarios.Usuario();
+            // Query
+            string sql = @"SELECT nombreUsuario, password FROM scc.Usuario WHERE nombreUsuario = @usuario and password = @contra";
+            // Ejecuatar el query
+            SqlCommand cmd = conexion.EjecutarComando(sql);
+            // Variables de los TextBox
+            string nombreUsuario = txtNombreUsuario.Text;
+            string password = txtPassword.Password;
+
+            // Leer los datos
+            SqlDataReader rdr;
+
+            try
+            {
+
+                using (cmd)
+                {
+                    // Ver los parametros
+                    cmd.Parameters.Add("@usuario", SqlDbType.VarChar, 255).Value = nombreUsuario;
+                    cmd.Parameters.Add("@contra", SqlDbType.VarChar, 255).Value = password;
+                    rdr = cmd.ExecuteReader();
+                }
+
+
+
+
+                // Leer los datos
+                while (rdr.Read())
+                {
+                    usuario.nombreUsuario = rdr.GetString(0);
+                    usuario.contrasena = rdr.GetString(1);
+
+                }
+
+                // Verificar que los TextBox no esten vacios
+                if (nombreUsuario == "")
+                {
+                    MessageBox.Show("Ingresa tu Nombre de Usuario", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (password == "")
+                {
+                    MessageBox.Show("Ingresa tu Contraseña", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                // Virificar si hay error en el usuario y la contrasena
+                else if (nombreUsuario != usuario.nombreUsuario || password != usuario.contrasena)
+                {
+                    MessageBox.Show("Usuario o Contraseña Incorrecto", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+                else
+                {   // Si no hayb errores en las credenciales 
+                    // Iniciar session
+                    Menu abrirMenu = new Menu();
+                    this.Hide();
+                    abrirMenu.ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+
+            }
+            finally
+            {
+
+                conexion.CerrarConexion();
+            }
+
+
+
+           
+
+
+            
+           
+
         }
     }
 }
---
+++ scc/Usuarios/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using MaterialDesignThemes.Wpf.Converters.CircularProgressBar;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace scc.Usuarios
+{
+    public class Usuario
+    {
+        // Meotodos de la clase Usuario
+        public int idUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contrasena { get; set; }
+
+
+     
+
+    }
+}
